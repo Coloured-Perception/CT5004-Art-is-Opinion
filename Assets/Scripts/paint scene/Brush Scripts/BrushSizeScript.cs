043b20c7dcbc3e2e76362a155ce9b21099fa4600
s1802879 <58692827+s1802879@users.noreply.github.com>
2020-08-21 12:41:45 +0100

    brush preview fix
    The brush preview is now the accurate size all the time. The normal brush is still disabled so you have to choose one from the menu before painting. While the prieview changes size with the buttons, the actual brush does not, so we need to change the code to use the brush preview image to change the brush whenever it or the size buttons are clicked, insted of changing when you select a button from the menu. Also the colour change no longer works.

--- Assets/Scripts/paint scene/Brush Scripts/BrushSizeScript.cs
+++ Assets/Scripts/paint scene/Brush Scripts/BrushSizeScript.cs
@@ -3,17 +3,10 @@ using UnityEngine.UI;
 
 namespace unitycoder_MobilePaint {
 	public class BrushSizeScript : MonoBehaviour {
-		// Current size of the brush and it's preview
-		float brushPreviewSize;
-		int currentBrushSize;
-
+	
 		// Size boundaries for brush
-		int minDifference = 3;
-		int maxDifference = 2;
-
-		//// Size boundaries for brush preview
-		//float maxPreviewSize = 2f;
-		//float minPreviewSize = 0.2f;
+		int maxDifference = 3;
+		int minDifference = -2;
 
 		public int customSize;
 
@@ -23,71 +16,57 @@ namespace unitycoder_MobilePaint {
 		MobilePaint mobilePaint;
 		public CustomBrushesUI customBrushesUI;
 		int sizeDifference;
+		string PreviewName;
 
-		// Start is called before the first frame update
 		void Start() {
 			mobilePaint = PaintManager.mobilePaint; // Gets reference to mobilePaint through PaintManager
-
-			// Sets the default Brush Size
-			currentBrushSize = 20;
-			mobilePaint.SetBrushSize(currentBrushSize);
-
-			//// sets the default for brush preview
-			//brushPreviewSize = 0.3f;
-			//brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
-
-			customSize = 3;
-			Debug.Log(customSize);
-
+					customSize = 2;
 		}
 
-		// Changes the size of the brush by 5
-		public void IncreaseBrushSize() {
-			Debug.Log(customSize);
-			// If the new size is bigger than the maxBrushSize, it won't change size
-			if (sizeDifference >= maxDifference - 1) {
-				// Increase brush size
-				currentBrushSize += 5;
-				mobilePaint.SetBrushSize(currentBrushSize);
-
-				//// Increase preview
-				//brushPreviewSize += 0.05f;
-				//brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
-
-
-				sizeDifference -= 1;
-
-				brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[customBrushesUI.sizeReference + sizeDifference];
-
-			}
-			
-				customSize -= 1;
-			Debug.Log(customSize);
-
-		}
-
-		// Changes the size of the brush by -5
 		public void DecreaseBrushSize() {
-			Debug.Log(customSize);
 
-			// If the new size is smaller than the minBrushSize, it won't change size
-			if (sizeDifference <= minDifference + 1) {
-				// Decrease brush size
-				currentBrushSize -= 5;
-				mobilePaint.SetBrushSize(currentBrushSize);
+			if (sizeDifference > minDifference) {
+				sizeDifference -= 1;
+				PreviewName = brushPreview.GetComponent<RawImage>().texture.name;
 
-				//// Decrease preview
-				//brushPreviewSize -= 0.05f;
-				//brushPreview.transform.localScale = new Vector2(brushPreviewSize, brushPreviewSize);
+				if (PreviewName.StartsWith("Circle")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 0 + sizeDifference];
+				} else if (PreviewName.StartsWith("Square")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 1 + sizeDifference];
+				} else if (PreviewName.StartsWith("Soft")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 2 + sizeDifference];
+				} else if (PreviewName.StartsWith("Splater")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 3 + sizeDifference];
+				} else if (PreviewName.StartsWith("Spoty")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 4 + sizeDifference];
+				} else if (PreviewName.StartsWith("Dots")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 5 + sizeDifference];
+				}
+				customSize -= 1;
+			}
+		}
 
+		public void IncreaseBrushSize() {
+
+			if (sizeDifference < maxDifference) {
 				sizeDifference += 1;
+				PreviewName = brushPreview.GetComponent<RawImage>().texture.name;
 
-				brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[customBrushesUI.sizeReference + sizeDifference];
-
+				if (PreviewName.StartsWith("Circle")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 0 + sizeDifference];
+				} else if (PreviewName.StartsWith("Square")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 1 + sizeDifference];
+				} else if (PreviewName.StartsWith("Soft")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 2 + sizeDifference];
+				} else if (PreviewName.StartsWith("Splater")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 3 + sizeDifference];
+				} else if (PreviewName.StartsWith("Spoty")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 4 + sizeDifference];
+				} else if (PreviewName.StartsWith("Dots")) {
+					brushPreview.GetComponent<RawImage>().texture = mobilePaint.customBrushes[2 + 6 * 5 + sizeDifference];
+				}
 				customSize += 1;
 			}
-			Debug.Log(customSize);
-
 		}
 	}
 }
