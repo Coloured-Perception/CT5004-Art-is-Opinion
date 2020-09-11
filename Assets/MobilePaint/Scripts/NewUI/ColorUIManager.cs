84a0e7239b6c9323a612deb45ee938fbaa8956db
KaneA23 <55718634+KaneA23@users.noreply.github.com>
2020-09-11 18:09:14 +0100

    Fixed Coloured Brush
    Found and uncommented code needed to change brush colour to be same as preview

--- Assets/MobilePaint/Scripts/NewUI/ColorUIManager.cs
+++ Assets/MobilePaint/Scripts/NewUI/ColorUIManager.cs
@@ -52,62 +52,62 @@ namespace unitycoder_MobilePaint
 			
 			preview.gameObject.GetComponent<RawImage>().color = newColor; // set current color image
 
-   //         // send new color
-   //         mobilePaint.SetPaintColor(newColor);
-   //         //mobilePaint.paintColor = newColor;
+			// send new color
+			mobilePaint.SetPaintColor(newColor);
+			//mobilePaint.paintColor = newColor;
 
-   //         //if (offsetSelected)
-   //         //{
-   //         //    ResetAllOffsets();
-   //         //    SetButtonOffset(button, moveOffsetX);
-   //         //}
+			//if (offsetSelected)
+			//{
+			//    ResetAllOffsets();
+			//    SetButtonOffset(button, moveOffsetX);
+			//}
 
-        }
-
-
-        //public void Update()
-        //{
-        //    if (Input.GetMouseButton(0))
-        //    {
-        //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        //        RaycastHit hit;
-
-        //        if (Physics.Raycast(ray, out hit))
-        //        {
-        //            var picker = hit.collider.GetComponent<ColorPicker>();
-
-        //            if (picker != null)
-        //            {
-        //                Renderer rend = hit.transform.GetComponent<Renderer>();
-        //                MeshCollider meshCollider = hit.collider as MeshCollider;
-
-        //                if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
-        //                    return;
-        //                Texture2D tex = rend.material.mainTexture as Texture2D;
-        //                Vector2 pixelUV = hit.textureCoord;
-        //                pixelUV.x *= tex.width;
-        //                pixelUV.y *= tex.height;
-        //                Color newColor = tex.GetPixel((int)pixelUV.x, (int)pixelUV.y);
-        //            }
-        //        }
-        //    }
-        //}
+		}
 
 
-        //      void ResetAllOffsets()
-        //{
-        //	for (int i=0;i<colorpickers.Length;i++)
-        //	{
-        //		SetButtonOffset(colorpickers[i],defaultOffset); 
-        //	}
-        //}
+		//public void Update()
+		//{
+		//    if (Input.GetMouseButton(0))
+		//    {
+		//        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//        RaycastHit hit;
+
+		//        if (Physics.Raycast(ray, out hit))
+		//        {
+		//            var picker = hit.collider.GetComponent<ColorPicker>();
+
+		//            if (picker != null)
+		//            {
+		//                Renderer rend = hit.transform.GetComponent<Renderer>();
+		//                MeshCollider meshCollider = hit.collider as MeshCollider;
+
+		//                if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+		//                    return;
+		//                Texture2D tex = rend.material.mainTexture as Texture2D;
+		//                Vector2 pixelUV = hit.textureCoord;
+		//                pixelUV.x *= tex.width;
+		//                pixelUV.y *= tex.height;
+		//                Color newColor = tex.GetPixel((int)pixelUV.x, (int)pixelUV.y);
+		//            }
+		//        }
+		//    }
+		//}
 
 
-        //void SetButtonOffset(Button button,float offsetX)
-        //{
-        //	RectTransform rectTransform = button.GetComponent<RectTransform>();
-        //	rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left,offsetX,rectTransform.rect.width);
-        //}
+		//      void ResetAllOffsets()
+		//{
+		//	for (int i=0;i<colorpickers.Length;i++)
+		//	{
+		//		SetButtonOffset(colorpickers[i],defaultOffset); 
+		//	}
+		//}
 
-    }   // class
+
+		//void SetButtonOffset(Button button,float offsetX)
+		//{
+		//	RectTransform rectTransform = button.GetComponent<RectTransform>();
+		//	rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left,offsetX,rectTransform.rect.width);
+		//}
+
+	}   // class
 }   // namespace
