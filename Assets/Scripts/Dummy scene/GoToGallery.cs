0d094f6f39c681df1eaed6f9d9cb4b1b2a1cad68
s1809503 <55702706+s1809503@users.noreply.github.com>
2021-06-03 19:35:47 +0100

    Eye-Tracker load stub scene
    Eye tracking only works if it has been loaded in a previous Scene. Added a stub scene that will load eye tracking and send to gallery.

---
+++ Assets/Scripts/Dummy scene/GoToGallery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Tobii.Gaming;
+
+public class GoToGallery : MonoBehaviour
+{
+    Vector2 filteredPoint;
+    public float headGazeContribution;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 gazePoint = TobiiAPI.GetGazePoint().Viewport;
+        filteredPoint = Vector2.Lerp(filteredPoint, gazePoint, (0.5f));
+        Quaternion headPose = TobiiAPI.GetHeadPose().Rotation;
+
+        Quaternion headPoseAngle = new Quaternion(headPose.x * headGazeContribution, headPose.y * headGazeContribution, 0, headPose.w);
+        Quaternion gazePointAngle = new Quaternion((-filteredPoint.y + 0.5f) * (1 - headGazeContribution), (filteredPoint.x - 0.5f) * (1 - headGazeContribution), 0, headPose.w);
+        SceneManager.LoadScene("GalleryScene");
+    }
+}
